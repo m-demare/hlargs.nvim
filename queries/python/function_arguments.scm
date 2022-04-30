97483f4e8d7ec5afd20c758417ec04fb46a257e4
Thomas MK <tmke@posteo.net>
2022-04-30 13:11:28 +0200

    Simplify queries and add test

--- queries/python/function_arguments.scm
+++ queries/python/function_arguments.scm
@@ -6,29 +6,23 @@
         (identifier) @argname)
       (dictionary_splat_pattern
         (identifier) @argname)
+      (default_parameter
+        [
+          (identifier) @argname
+        ])
+      (typed_parameter
+        [
+          (identifier) @argname
+          (list_splat_pattern
+            (identifier) @argname)
+          (dictionary_splat_pattern
+            (identifier) @argname)
+        ])
+      (typed_default_parameter
+        [
+          (identifier) @argname
+        ])
     ]))
-(function_definition
-  parameters: (parameters
-    (default_parameter
-      [
-        (identifier) @argname
-      ])))
-(function_definition
-  parameters: (parameters
-    (typed_parameter
-      [
-        (identifier) @argname
-        (list_splat_pattern
-          (identifier) @argname)
-        (dictionary_splat_pattern
-          (identifier) @argname)
-      ])))
-(function_definition
-  parameters: (parameters
-    (typed_default_parameter
-      [
-        (identifier) @argname
-      ])))
 (lambda
   parameters: (lambda_parameters
     [
@@ -37,4 +31,8 @@
         (identifier) @argname)
       (dictionary_splat_pattern
         (identifier) @argname)
+      (default_parameter
+        [
+          (identifier) @argname
+        ])
     ]))
