b8bcbc6142d74adce8c8c9062c84fd387ad8f58e
jmarkin <me@jmarkin.ru>
2022-07-07 23:38:44 +0300

    feat(colorpalette): change MAX_INT type number -> int64

--- lua/hlargs/colorpalette.lua
+++ lua/hlargs/colorpalette.lua
@@ -6,14 +6,14 @@ local M = {}
 -- https://cp-algorithms.com/string/string-hashing.html
 local P = 103
 --math.maxinteger lua >= 5.3
-local MAX_INT = 9223372036854775807
+local MAX_INT = 9223372036854775807LL
 
 local hash_arg = function(arg_name)
     local hash_value = 0
     local p_pow = 1
     for i=1,#arg_name do
         local ascii = arg_name:byte(i)
-        hash_value = (hash_value + ascii *p_pow) % MAX_INT
+        hash_value = (hash_value + ascii * p_pow) % MAX_INT
         p_pow = (p_pow * P) % MAX_INT
     end
     return hash_value
@@ -25,7 +25,7 @@ M.get_hlgroup = function(arg_name)
   local idx = hash_arg(arg_name)
   local current_color = idx % size
 
-  return "Hlarg" .. current_color
+  return "Hlarg" .. tonumber(current_color)
 end
 
 return M
