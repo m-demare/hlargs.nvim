a7ad6ed8d6e27ea4dd13fda63fa732e9196ba4ea
Dema <34817965+m-demare@users.noreply.github.com>
2023-03-06 14:53:24 -0300

    fix: callbacks stayed attached after a ft change even when they shouldn't
    fix: some weird race condition in timer functions caused callbacks to be
    executed even after cancelled. This caused parsing tasks to be created
    even after a buffer became invalid

---
+++ lua/hlargs/async.lua
@@ -0,0 +1,38 @@
+local function attach(bufnr, callabcks)
+  local detached = false
+
+  vim.api.nvim_buf_attach(bufnr, false, {
+    on_lines = function(...)
+      if detached then return true end
+      callabcks.on_lines(...)
+    end,
+    on_reload = function(...)
+      if detached then return true end
+      callabcks.on_reload(...)
+    end
+  })
+
+  return function()
+    detached = true
+  end
+end
+
+local function defer(fn, time)
+  local cancelled = false
+  local t = vim.defer_fn(function ()
+    if cancelled then return end
+    fn()
+  end, time)
+  return function ()
+    cancelled = true  -- It seems like there's some sort of race condition with these
+                      -- timers, they occasionally get executed after being cancelled.
+                      -- This flag prevents that behaviour.
+    vim.loop.timer_stop(t)
+  end
+end
+
+return {
+  attach = attach,
+  defer = defer,
+}
+
