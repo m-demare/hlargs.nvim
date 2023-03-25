d660332d0017f1401626528ac27a2f080b69c38a
Andrew Moore <andrew@lessthanseventy.com>
2023-03-25 09:46:27 -0600

    update deprecated functions

--- lua/hlargs/parse.lua
+++ lua/hlargs/parse.lua
@@ -1,40 +1,44 @@
 local M = {}
 
 local ts = vim.treesitter
-local config = require 'hlargs.config'
-local util = require 'hlargs.util'
+local config = require("hlargs.config")
+local util = require("hlargs.util")
 
 -- If arguments were modified, the whole function has to be reparsed
 local function fix_mark(bufnr, marks_ns, root_node, mark)
   local lang = util.get_lang(bufnr)
-  local query = ts.query.get_query(lang, 'function_arguments')
+  local query = ts.query.get(lang, "function_arguments")
   local orig_from, orig_to = util.get_marks_limits(bufnr, marks_ns, mark)
   local new_from, new_to = orig_from, orig_to
-  for id, node in query:iter_captures(root_node, bufnr, orig_from, orig_to+1) do
+  for id, node in query:iter_captures(root_node, bufnr, orig_from, orig_to + 1) do
     local start_row, _, end_row, _ = util.get_first_function_parent(lang, node):range()
-    if start_row < new_from then new_from = start_row end
-    if end_row > new_to then new_to = end_row end
+    if start_row < new_from then
+      new_from = start_row
+    end
+    if end_row > new_to then
+      new_to = end_row
+    end
   end
   vim.api.nvim_buf_set_extmark(bufnr, marks_ns, new_from, 0, {
     id = mark,
     end_row = new_to,
-    end_col = 0
+    end_col = 0,
   })
 end
 
 function M.get_args(bufnr, func_node)
   local lang = util.get_lang(bufnr)
-  local query = ts.query.get_query(lang, 'function_arguments')
+  local query = ts.query.get(lang, "function_arguments")
 
   local start_row, _, end_row, _ = func_node:range()
   local arg_names_set, arg_nodes = {}, {}
-  for id, node in query:iter_captures(func_node, bufnr, start_row, end_row+1) do
+  for id, node in query:iter_captures(func_node, bufnr, start_row, end_row + 1) do
     local capture_name = query.captures[id]
     if util.get_first_function_parent(lang, node) == func_node then
-      if capture_name ~= 'catch' or config.opts.paint_catch_blocks.declarations then
+      if capture_name ~= "catch" or config.opts.paint_catch_blocks.declarations then
         table.insert(arg_nodes, node)
       end
-      local arg_name = vim.treesitter.query.get_node_text(node, bufnr)
+      local arg_name = vim.treesitter.get_node_text(node, bufnr)
       arg_names_set[arg_name] = capture_name
     end
   end
@@ -44,38 +48,43 @@ end
 
 function M.get_body_nodes(bufnr, func_node)
   local lang = util.get_lang(bufnr)
-  local query = ts.query.get_query(lang, 'function_body')
+  local query = ts.query.get(lang, "function_body")
 
   local start_row, _, end_row, _ = func_node:range()
   local nodes = {}
-  local i=0
+  local i = 0
   for id, node in query:iter_captures(func_node, bufnr, start_row, end_row + 1) do
-    if i==0 or util.get_first_function_parent(lang, node) == func_node then
+    if i == 0 or util.get_first_function_parent(lang, node) == func_node then
       table.insert(nodes, node)
       if not util.is_multi_body_lang(lang) then
         return nodes
       end
     end
-    i = i+1
+    i = i + 1
   end
   return nodes
 end
 
 function M.get_arg_usages(bufnr, body_nodes, arg_names_set, limits)
   local lang = util.get_lang(bufnr)
-  local query = ts.query.get_query(lang, 'variables')
+  local query = ts.query.get(lang, "variables")
 
   local usages_nodes = {}
   for _, body_node in ipairs(body_nodes) do
     local start_row, _, end_row, _ = body_node:range()
-    if limits then start_row, end_row = limits[1], limits[2] end
+    if limits then
+      start_row, end_row = limits[1], limits[2]
+    end
 
-    for id, node in query:iter_captures(body_node, bufnr, start_row, end_row+1) do
+    for id, node in query:iter_captures(body_node, bufnr, start_row, end_row + 1) do
       local capture_name = query.captures[id]
-      if capture_name ~= 'ignore' then
-        local arg_name = vim.treesitter.query.get_node_text(node, bufnr)
-        if arg_names_set[arg_name] and not util.ignore_node(lang, node)
-          and (arg_names_set[arg_name] ~= 'catch' or config.opts.paint_catch_blocks.usages)then
+      if capture_name ~= "ignore" then
+        local arg_name = vim.treesitter.get_node_text(node, bufnr)
+        if
+            arg_names_set[arg_name]
+            and not util.ignore_node(lang, node)
+            and (arg_names_set[arg_name] ~= "catch" or config.opts.paint_catch_blocks.usages)
+        then
           table.insert(usages_nodes, node)
         end
       end
@@ -85,14 +94,14 @@ function M.get_arg_usages(bufnr, body_nodes, arg_names_set, limits)
 end
 
 local function not_excluded_name(bufnr, excluded_names)
-  return function (node)
-    return not vim.tbl_contains(excluded_names, vim.treesitter.query.get_node_text(node, bufnr))
+  return function(node)
+    return not vim.tbl_contains(excluded_names, vim.treesitter.get_node_text(node, bufnr))
   end
 end
 
 function M.get_nodes_to_paint(bufnr, marks_ns, mark, on_changedtree)
   local lang = util.get_lang(bufnr)
-  local query = ts.query.get_query(lang, 'function_definition')
+  local query = ts.query.get(lang, "function_definition")
   if query == nil then
     return
   end
@@ -101,9 +110,9 @@ function M.get_nodes_to_paint(bufnr, marks_ns, mark, on_changedtree)
   local syntax_tree = parser:parse()
   local root = syntax_tree[1]:root()
   if on_changedtree then
-    parser:register_cbs {
-      on_changedtree = on_changedtree
-    }
+    parser:register_cbs({
+      on_changedtree = on_changedtree,
+    })
   end
 
   local start_row, _, end_row, _ = root:range()
@@ -114,21 +123,21 @@ function M.get_nodes_to_paint(bufnr, marks_ns, mark, on_changedtree)
 
   local i = 0
   for id, node in query:iter_captures(root, bufnr, start_row, end_row) do
-    i = i+1
+    i = i + 1
     if config.opts.performance.max_iterations > 0 and i > config.opts.performance.max_iterations then
       return
     end
     local name = query.captures[id] -- name of the capture
     local arg_nodes, arg_names_set = M.get_args(bufnr, node)
     local usages_nodes = {}
-    if config.opts.paint_arg_usages and #arg_nodes>0 then
+    if config.opts.paint_arg_usages and #arg_nodes > 0 then
       local body_nodes = M.get_body_nodes(bufnr, node)
       local limits = nil
       if mark then
         local from, to = util.get_marks_limits(bufnr, marks_ns, mark)
         limits = { from, to }
       end
-      if body_nodes and #body_nodes>0 then
+      if body_nodes and #body_nodes > 0 then
         -- So that empty functions don't fail
         usages_nodes = M.get_arg_usages(bufnr, body_nodes, arg_names_set, limits)
       end
@@ -144,4 +153,3 @@ function M.get_nodes_to_paint(bufnr, marks_ns, mark, on_changedtree)
 end
 
 return M
-
