cc236ae49f245d75c64a8d168f14c84d631530e7
231tr0n <zeltronsrikar@gmail.com>
2025-09-21 20:16:23 +0530

    feat: update java testfile

--- testfiles/test.java
+++ testfiles/test.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
 public class Class {
-    private final static Long arg2 = 25L;
+  private static final Long arg2 = 25L;
+  private static int arg1 = 25;
 
-    private static int fn(Arg arg1, int arg2) {
-        return arg1.arg1() * Class.arg2.intValue() + arg2;
-    }
+  public Class(int arg1) {
+    Class.arg1 = arg1;
+  }
 
-    private static Optional<Builder> build(int field, Optional<Integer> anotherFieldOptional) {
-        return anotherFieldOptional.map(
-                anotherField ->
-                    Builder.builder()
-                        .field(field)
-                        .anotherField(anotherField)
-                        .build())
-    }
+  private static int fn(Arg arg1, int arg2) {
+    return arg1.arg1() * Class.arg2.intValue() + arg2;
+  }
 
-    public static void main(String []args) {
-        List<String> strs = Arrays.asList(args);
-        try {
-            strs.sort((String str1, String str2) -> str1.compareTo(str2));
-            System.out.println(strs);
-        } catch (Exception e) {
-            System.err.println(e);
-        }
+  private static Optional<Builder> build(int field, Optional<Integer> anotherFieldOptional) {
+    return anotherFieldOptional.map(
+        anotherField -> Builder.builder().field(field).anotherField(anotherField).build());
+  }
+
+  public static void main(String[] args) {
+    List<String> strs = Arrays.asList(args);
+    try {
+      strs.sort((String str1, String str2) -> str1.compareTo(str2));
+      System.out.println(strs);
+    } catch (Exception e) {
+      System.err.println(e);
     }
+  }
 }
-
